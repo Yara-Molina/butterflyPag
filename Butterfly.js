d8d43cf83429866ec38438b7805268c11e0e02de
Yara-Molina <147678241+Yara-Molina@users.noreply.github.com>
2024-05-27 00:14:18 -0600

    Add files via upload

---
+++ Butterfly.js
@@ -0,0 +1,17 @@
+class Butterlfy{
+    scienceName
+    simpleName
+    gender
+    habit
+    family
+    constructor(scienceName,simpleName,gender,family){
+        this.scienceName= scienceName
+        this.simpleName = simpleName
+        this.gender = gender
+        this.habit = habit
+        this.family = family
+    }
+
+}
+
+export default Butterlfy
